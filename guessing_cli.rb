7672ec0dc91506d72491f86f7860ab6f1d8576e0
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 19:37:42 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,12 +1,18 @@
 def run_guessing_game
   puts "Hello, welcome to the Guessing Game! Guess a number between 1 and 10!"
   user_guess = gets.strip
+  while user_guess !=
+  
+  
+  
+  
   if (1..10).to_a.include?(user_guess)
     comp_guess = rand(1..10)
-    #code  
+     
   elsif user_guess == "exit"
-     puts: "Goodbye!"
      break
+     puts: "Goodbye!"
+    
   end
   
   
