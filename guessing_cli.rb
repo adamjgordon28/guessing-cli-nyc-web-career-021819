80ebc79c8cff2c3a4d221a5cdd7bd2216b037949
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 19:40:40 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,31 +1,10 @@
-def prompt ()
-  puts "Please make another guess!"
-  user_input = gets.chomp
-end
-
-
 def run_guessing_game
   puts "Hello, welcome to the Guessing Game! Guess a number between 1 and 10!"
-  user_guess = gets.chomp
-  
-  
-  
-  
-  if (1..10).to_a.include?(user_guess)
-    comp_guess = rand(1..10)
-     
-  elsif user_guess == "exit"
-     break
-     puts: "Goodbye!"
-    
-  end
-  
-  
-  
-  
-  if user_guess == comp_guess 
+  user_guess = gets.strip
+  comp_guess = rand(1..10)
+  if user_guess.to_i == comp_guess 
     puts "You guessed the correct number!"
-  elsif user_gues
+  elsif user_
     puts "The computer guessed #{comp_guess}."
   end
 end
