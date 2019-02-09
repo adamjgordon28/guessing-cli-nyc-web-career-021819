24e83fe91c4b2a6fc11b2dd189cb50a0db42f654
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 19:42:52 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,8 @@ def run_guessing_game
   comp_guess = rand(1..10)
   if user_guess.to_i == comp_guess 
     puts "You guessed the correct number!"
-  elsif user_
+  elsif user_guess != comp_guess
     puts "The computer guessed #{comp_guess}."
+  elsif user_guess == "exit"
   end
 end
