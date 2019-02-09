c8faa693a765015d1b1de80ee8dbf9fac5da504e
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 19:43:41 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_guess = gets.strip
-  comp_guess = rand(1..10)
+  comp_guess = rand(1..6)
   if user_guess.to_i == comp_guess 
     puts "You guessed the correct number!"
   elsif user_guess != comp_guess
